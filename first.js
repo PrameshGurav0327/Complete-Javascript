d99a28609b6481247b0dc949a5d989b1a4094349
pramesh gurav <prameshgurav79286@gmail.com>
2025-06-11 11:55:37 +0530

    Update

--- first.js
+++ first.js
@@ -1,6 +1,74 @@
-fullName = "Tony Stark";
-price = 99.99;
-x = null
-console.log(fullName);  
-console.log(price);  
-console.log(x);  
+// ============ Variables in Javascript ==================
+
+// This two will be declare different variables in JS 
+// 👇
+
+// fullName = "Tony Stark";
+// FULLNAME = "Pramesh Gurav";
+// console.log(fullName);
+// console.log(FULLNAME);
+
+// ============ let,const & var ======================
+
+// var : can be redeclared and updated 
+// let : cannot be redeclared but can be updated 
+// const : cannot be redeclared and updated 
+
+// const fullname = "Tony Stark";
+// let fullname = "Tony Stark";
+// var fullname = "Tony Stark";
+// fullname = "Thor";
+// fullname = "Steve";
+
+// console.log(fullname);
+
+// ============= Data types in JS =================
+
+// Primitive datatypes : These datatypes are fixed datatypes
+
+// 1. number
+// const a = 25:
+
+// 2. string
+// let fullName = "Tony Stark";
+// typeof ..... : 👈🏼 this will tell you name of datatype
+
+// 3. Boolean
+// isFollow = true;
+
+// 4. Undefined
+// let P;
+
+// 5. Null
+// let x = null;
+
+// BigInt
+// let x = BigInt("123456");
+
+// Symbol
+// let y = Symbol("Hello!");
+
+// const Student = {
+//     FullName: "Steve Roger",
+//     age : 107,
+//     cgpa : 6.5,
+//     isPass : true
+// };
+
+// console.log(Student["FullName"])
+// console.log(Student.cgpa);
+
+// {
+//     let a = 5;
+//     // let a = 6; {cannot redeclare in the same block}
+//     console.log(a);
+// }
+
+// But can be redeclare in new block 
+
+// {
+//     let a = 99;
+//     console.log(a);
+// }
+
+// ============== 
